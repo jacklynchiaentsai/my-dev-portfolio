31ae76a0a55de9e7af785d6d580ab52c03a690ae
jacklynchiaentsai <ct3064@columbia.edu>
2023-05-19 02:13:20 -0400

    updated technical skills section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { technologies } from "../constants"
+
+// for incorporating title in tech section
+import {motion} from 'framer-motion'
+import {fadeIn, textVariant} from '../utils/motion'
+import {styles} from '../styles'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <>
+    <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>Technical Skills</h2>
+    </motion.div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {
+        technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+            <p className="text-center">{technology.name}</p>
+          </div>
+        )
+
+        )
+      }
+    </div>
+    </>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech,"")

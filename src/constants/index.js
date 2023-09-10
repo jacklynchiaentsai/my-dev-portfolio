11808d694b31b3c513f2518ebab087d6fedcecda
jacklynchiaentsai <jacklyntsai1213@gmail.com>
2023-09-10 14:09:44 -0400

    debug code

--- src/constants/index.js
+++ src/constants/index.js
@@ -412,4 +412,34 @@ import {
     },
   ];
   
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "CU Engineers Without Borders: Uganda Chapter",
+      date: "September 2021 - Present",
+      title: "Grants & Fundraising Team Lead",
+      image: ewb,
+      link: "https://www.instagram.com/cuewbuganda/",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "CU Taiwanese American Student Association",
+      date: "September 2021 - Present",
+      title: "Social Media Chair",
+      image: tasa,
+      link: "https://www.instagram.com/tasa_cu/",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "CU Naginata Club",
+      date: "September 2022 - Present",
+      title: "Member (3-kyu)",
+      image: naginata,
+      link: "https://www.instagram.com/cunaginata/",
+    },
+  ];
+  
   export { services, technologies, experiences, testimonials, projects, languages };
